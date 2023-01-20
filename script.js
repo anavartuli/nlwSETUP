e5d00b3e10f1c3612d73ae1c96d0428ee5d1c97a
ana carolina <vartuliana@gmail.com>
2023-01-20 10:50:27 -0300

    added functions to register days and save data

--- script.js
+++ script.js
@@ -4,14 +4,29 @@ const form = document.querySelector('#form-habits')
 // iniciando a biblioteca
 const nlwSetup = new NLWSetup(form)
 
-const data = {
-    water: ['01-01', '01-02', '01-03', '01-07'],
-    study:['01-01', '01-02', '01-03', '01-04', '01-07'],
-    healthy: ['01-04'],
-    sleep: ['01-01', '01-06'],
-    meditate: [],
-    pills: ['01-01', '01-03', '01-05']
+// funções para botão de registro do dia e salvar dados
+const button = document.querySelector('header button')
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add(){
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if (dayExists){
+        alert("Dia já incluso 🌞")
+        return
+    }
+
+    alert('Dia adicionado com sucesso ✔️')
+    nlwSetup.addDay(today)
 }
+
+function save(){
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
